8236683cea62b36849615f58bf30b6dd04fa5f96
Joe Buhlig <hello@joebuhlig.com>
2017-10-19 09:30:04 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-rzim-customizations
+# about: Adds tweaks to RZIM Connect
+# version: 0.1
+# author: Joe Buhlig joebuhlig.com
+# url: https://www.github.com/joebuhlig/discourse-rzim-customizations
+
+enabled_site_setting :discourse_rzim_customizations_enabled
